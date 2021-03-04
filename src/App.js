046726b2ca557e4ea265b7b4c01f7066716dbac5
stephenPC <stephenmissah@outlook.com>
2021-03-04 16:16:12 +0000

    added images in source and styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="app"></div>;
+import React from "react";
+import Jumbotron from "./components/jumbotron";
+import jumboData from "./fixtures/jumbo.json";
+export default function App() {
+  return (
+    <div className="app">
+      <Jumbotron.Container>
+        {jumboData.map(({ id, title, subTitle, image, alt, direction }) => {
+          return (
+            <Jumbotron key={id} direction={direction}>
+              <Jumbotron.Pane>
+                <Jumbotron.Title>{title}</Jumbotron.Title>
+                <Jumbotron.Subtitle>{subTitle}</Jumbotron.Subtitle>
+              </Jumbotron.Pane>
+              <Jumbotron.Image src={image} alt={alt} />
+            </Jumbotron>
+          );
+        })}
+      </Jumbotron.Container>
+    </div>
+  );
 }
-
-export default App;

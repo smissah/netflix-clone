1d78567d2cf22f3871e2f9f3fdf97e3a30c39955
stephenPC <stephenmissah@outlook.com>
2021-03-05 13:27:24 +0000

    added one source of truth for the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React from "react";
-
-import Jumbotron from "./components/jumbotron";
-import jumboData from "./fixtures/jumbo.json";
+import { JumbotronContainer } from "./containers/jumbotron";
 export default function App() {
-  return (
-    <div className="app">
-      <Jumbotron.Container>
-        {jumboData.map(({ id, title, subTitle, image, alt, direction }) => {
-          return (
-            <Jumbotron key={id} direction={direction}>
-              <Jumbotron.Pane>
-                <Jumbotron.Title>{title}</Jumbotron.Title>
-                <Jumbotron.Subtitle>{subTitle}</Jumbotron.Subtitle>
-              </Jumbotron.Pane>
-              <Jumbotron.Image src={image} alt={alt} />
-            </Jumbotron>
-          );
-        })}
-      </Jumbotron.Container>
-    </div>
-  );
+  return <JumbotronContainer />;
 }
---
+++ src/containers/jumbotron.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Jumbotron } from "../components";
+import jumboData from "../fixtures/jumbo.json";
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      {jumboData.map(({ id, title, subTitle, image, alt, direction }) => {
+        return (
+          <Jumbotron key={id} direction={direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{title}</Jumbotron.Title>
+              <Jumbotron.Subtitle>{subTitle}</Jumbotron.Subtitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Image src={image} alt={alt} />
+          </Jumbotron>
+        );
+      })}
+    </Jumbotron.Container>
+  );
+}

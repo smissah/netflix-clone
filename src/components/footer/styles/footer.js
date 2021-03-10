6effbf933ae3a93a06464eff006e2ccf3319434e
stephenPC <stephenmissah@outlook.com>
2021-03-10 16:46:55 +0000

    footer completed - space issue fixed. Accordian start T1.30

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -28,7 +28,7 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
   grid-gap: 1.5rem;
 
   @media (max-width: 1000px) {

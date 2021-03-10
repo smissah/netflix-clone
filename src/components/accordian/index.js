7324b44b2b3d033a60dbd0c0f43b4ab1a26d98a0
stephenPC <stephenmissah@outlook.com>
2021-03-10 17:13:15 +0000

    accordian started - need to add acc. styled components for compound component

---
+++ src/components/accordian/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext, createContext } from "react";
+export default function Accordian({ children, ...restProps }) {
+  const ToggleContext = createContext();
+  //context provider so headers can have access to state of item
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordian.Title = function AccordianTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordian.Frame = function AccordianFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordian.Item = function AccordianItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+  return;
+  <ToggleContext.Provider value={(toggleShow, setToggleShow)}>
+    <Item {...restProps}> {children}</Item>
+  </ToggleContext.Provider>;
+};
+
+Accordian.Header = function AccordianHeader({ chilren, ...restProps }) {
+  //destructor the togglecontext provider here
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      //above means take the previous take and reserve it. Looks inefficient but deals with the issue of use clicking many times and not giving react time to adjust
+      {...restProps}
+    >
+      {chilren}
+    </Header>
+  );
+};
